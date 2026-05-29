022a103510bde21b9be81ae856cea46e9ae39dd7
IgnacioDiaz7 <ign.diazv@duocuc.cl>
2026-05-28 22:23:33 -0400

    falta crear perfil de staff para ver sus ganancias

--- smartbooking_hub/users/urls.py
+++ smartbooking_hub/users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import home_view, registro_dueno
+from .views import home_view, registro_dueno, verificar_correo, dashboard, login_user, logout_user, pantalla_test
 
 urlpatterns = [
-    # Ruta para renderizar la página web de inicio
+    # Rutas originales
     path('', home_view, name='home'),
     path('registro-dueno/', registro_dueno, name='registro_dueno'),
+    path('verificar/<uidb64>/<token>/', verificar_correo, name='verificar_correo'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('test/', pantalla_test, name='pantalla_test'),
 ]
